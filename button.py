cf577f8170cee224c492f1a8f37e7058a7ebe73a
drapers2 <drapers2@carleton.edu>
2023-05-24 14:05:13 -0500

    added button

---
+++ button.py
@@ -0,0 +1,58 @@
+from graphics import *
+
+import utilities
+
+'''
+The Button class represents one of the buttons on the right-hand
+margin of the window.  No buttons are predefined by the board; it
+is up to the Game class to tell the Board to create the buttons.
+'''
+class Button:
+
+    button_width = 100
+    button_height = 30
+
+    '''
+    Set the size for all future buttons.  This is useful when the
+    window is mapped to non-pixel coordiantes.  This is not an
+    instance method, it is a class method, so it's called like
+    this:
+        button.Button.set_button_size(w,h)
+    '''
+    def set_button_size(width, height):
+        Button.button_width = width
+        Button.button_height = height
+
+    '''
+    Create a button at the given position (lower left),
+    with the given text.
+    '''
+    def __init__(self, lower_left, text):
+        x = lower_left.getX()
+        y = lower_left.getY()
+        self.button_rect = Rectangle(
+            lower_left,
+            Point(x + self.button_width, y + self.button_height))
+        self.button_text = Text(Point(x + self.button_width / 2, y + self.button_height / 2), text)
+
+    '''
+    Draw the button on the given GraphWin object.
+    '''
+    def draw(self, win):
+        self.button_rect.draw(win)
+        self.button_text.draw(win)
+
+    '''
+    Return the text for this button.
+    '''
+    def text(self):
+        return self.button_text.getText()
+
+    '''
+    Return True if the given point is in the button,
+    False otherwise.
+    '''
+    def click(self, pt):
+        return utilities.pt_in_rect(pt, self.button_rect)
+
+
